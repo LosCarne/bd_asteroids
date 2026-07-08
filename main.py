ac9a7f65c4f5612b15f3305528396d01bed8055e
LosCarne <carniex_99@outlook.com>
2026-07-08 14:22:43 -0400

    E: added circles and player

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
+from player import Player
 
 def main():
     pygame.init()
@@ -11,16 +11,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,"BLACK")
+        screen.fill("BLACK")
+        player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()

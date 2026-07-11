1de1aad37572a603863faf2f1c340767b00b47b2
LosCarne <carniex_99@outlook.com>
2026-07-10 20:45:21 -0400

    G: finished player object and added asteroids.

--- main.py
+++ main.py
@@ -2,29 +2,53 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    asteroid = AsteroidField() 
+
     while True:
-        log_state()
+        log_state()   # log events. 
+
+        # quits the game when the x is pressed.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # fills the background color    
         screen.fill("BLACK")
-        #player.update(dt)
-        player.draw(screen)
-        player.update(dt)
+
+        # updates all items in updatable group
+        updatable.update(dt)
+        
+        # used to draw each item in the drawable group
+        for item in drawable:
+            item.draw(screen)
+
+        # updates the screen
         pygame.display.flip()
+
+        # sets delta time.
         dt = clock.tick(60) / 1000
         
+    pygame.QUIT
 
 if __name__ == "__main__":
     main()
